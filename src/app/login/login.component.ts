7c6597d91bac0970025257e07a7b980245613a14
chezeauo <olivier.chezeaubernard@gmail.com>
2017-11-20 21:35:01 +0100

    add auth error message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation } from "@angular/core";
 import { Credentials } from "../app.model";
 import { AngularFireAuth } from "angularfire2/auth";
 import { Router } from "@angular/router";
+import { NotifService } from "../service/notif.service";
 
 @Component({
   selector: "app-login",
@@ -12,14 +13,14 @@ import { Router } from "@angular/router";
 export class LoginComponent {
   public credentials = new Credentials();
 
-  constructor(public afAuth: AngularFireAuth, private router: Router) {
+  constructor(public afAuth: AngularFireAuth, private router: Router, private notifService: NotifService) {
 
   }
 
   public doLogin(): void {
     this.afAuth.auth.signInWithEmailAndPassword(this.credentials.login, this.credentials.password).then(r => {
       this.router.navigate(["/"]);
-    }).catch(r => console.log(r));
+    }).catch(r => this.notifService.show("Erreur d'authentification"));
   }
 
 }
